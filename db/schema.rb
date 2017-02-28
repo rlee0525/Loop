afc72569f4bd598df22d46b0e92e9bdfbc7214cd
Raymond Lee <raylee0525@gmail.com>
2017-02-27 21:18:04 -0800

    Modified users table to include more information

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223022025) do
+ActiveRecord::Schema.define(version: 20170228051520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20170223022025) do
     t.string   "session_token",   null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "level"
+    t.integer  "experience"
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
