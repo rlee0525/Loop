190ed484f6aa3de0d3e631616d3311c2b3acd96f
Raymond Lee <raylee0525@gmail.com>
2017-02-27 21:32:19 -0800

    Update user json and controller to accomodate more information

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -13,6 +13,6 @@ class Api::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :email, :first_name, :last_name, :experience, :level)
   end
 end
--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! user, :id, :username
+json.extract! user, :id, :username, :email, :level, :experience, :first_name, :last_name
