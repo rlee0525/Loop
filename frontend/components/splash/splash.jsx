9792d448a88ea093afcd5689fbb4aa2ba967aa98
Raymond Lee <raylee0525@gmail.com>
2017-03-13 23:52:09 -0700

    CSS styling update with search bar container

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -24,17 +24,56 @@ class Splash extends React.Component {
           <div className="banner">
             <h1>
               <span id="title">Which event?</span>
-              Connect with next person<br/>on Loop.
+              Connect with next person<br/>on Loop<span id="title">.</span>
             </h1>
-            <section className="search-bar-section">
-              <input type="text"
-                placeholder="Search for an event"
-                className="search-bar" />
-            </section>
+
+            <div className="search-bar-container">
+              <div className="search-bar-section">
+                <div className="search-container">
+                  <div className="search-title">
+                    <p>Category</p>
+                  </div>
+                  <div className="search-input">
+                    <input type="text"
+                      placeholder="All"
+                      className="search-bar" />
+                  </div>
+                </div>
+
+                <div className="search-container">
+                  <div className="search-title">
+                    <p>Activity</p>
+                  </div>
+                  <div className="search-input">
+                    <input type="text"
+                      placeholder="Anything"
+                      className="search-bar" />
+                  </div>
+                </div>
+
+                <div className="search-container">
+                  <div className="search-title">
+                    <p>Date</p>
+                  </div>
+
+                  <div className="search-input">
+                    <input type="text"
+                      placeholder="Anytime"
+                      className="search-bar" />
+                  </div>
+
+                  <div className="search-button">
+                    <button>Search</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
+
           <div className="splash-main">
 
           </div>
+
           <div className="footer">
 
           </div>
