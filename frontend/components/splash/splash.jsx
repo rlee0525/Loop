92b69e830f88d4290af8baabf1e4f42cd0b60cfb
Raymond Lee <raylee0525@gmail.com>
2017-03-14 22:59:46 -0700

    Modular - separate main sliders / carousels

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -4,6 +4,9 @@ import FontAwesome from 'react-fontawesome';
 import Navbar from '../navbar/navbar_container';
 import MainSearch from '../main_search/main_search_container';
 import Slider from 'react-slick';
+import FeaturedSlider from '../main_sliders/featured_slider';
+import HottestSlider from '../main_sliders/hottest_slider';
+import BookedSlider from '../main_sliders/booked_slider';
 
 class Splash extends React.Component {
   constructor(props) {
@@ -43,43 +46,6 @@ class Splash extends React.Component {
   }
 
   render() {
-    let nextArrow = this.nextArrow(this.props);
-    let prevArrow = this.prevArrow(this.props);
-
-    let settings = {
-      dots: false,
-      arrows: true,
-      nextArrow: nextArrow,
-      prevArrow: prevArrow,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      initialSlide: 0,
-      responsive: [{
-        breakpoint: 1350,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: false,
-        }
-      }, {
-        breakpoint: 1000,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      }, {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }]
-    };
-
     return (
       <div>
         <Navbar />
@@ -103,60 +69,9 @@ class Splash extends React.Component {
             </div>
 
             <div className="splash-main">
-              <div className="featured-div">
-                <div className="featured-title">
-                  Featured concerts
-                </div>
-                <div className="featured-slider">
-                  <Slider className="slider-container" {...settings}>
-                    <img className="img-slider" src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla5_nuqs5l.jpg"/>
-                    <img className="img-slider" src="http://res.cloudinary.com/rlee0525/image/upload/v1489478256/lolla7_bgd2nk.jpg"/>
-                    <img className="img-slider" src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla1_kd83qs.jpg"/>
-                    <img className="img-slider" src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla2_nep093.jpg"/>
-                    <img className="img-slider" src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla3_x9hooi.jpg"/>
-                    <img className="img-slider" src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla4_cskppb.jpg"/>
-                    <img className="img-slider" src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla6_savm0g.jpg"/>
-                    <img className="img-slider" src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla8_qj9fpq.jpg"/>
-                    <img className="img-slider" src="http://res.cloudinary.com/rlee0525/image/upload/v1489478256/lolla9_tpj90y.jpg"/>
-                  </Slider>
-                </div>
-              </div>
-              <div className="booked-div">
-                <div className="featured-title">
-                  Just booked
-                </div>
-                <div className="featured-slider">
-                  <Slider className="slider-container" {...settings}>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla6_savm0g.jpg"/>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla2_nep093.jpg"/>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla3_x9hooi.jpg"/>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla4_cskppb.jpg"/>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla5_nuqs5l.jpg"/>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478256/lolla7_bgd2nk.jpg"/>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla8_qj9fpq.jpg"/>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478256/lolla9_tpj90y.jpg"/>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla1_kd83qs.jpg"/>
-                  </Slider>
-                </div>
-              </div>
-              <div className="activities-div">
-                <div className="featured-title">
-                  Activities
-                </div>
-                <div className="featured-slider">
-                  <Slider className="slider-container" {...settings}>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla6_savm0g.jpg"/>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla2_nep093.jpg"/>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla3_x9hooi.jpg"/>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla4_cskppb.jpg"/>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla5_nuqs5l.jpg"/>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478256/lolla7_bgd2nk.jpg"/>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla8_qj9fpq.jpg"/>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478256/lolla9_tpj90y.jpg"/>
-                    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489478255/lolla1_kd83qs.jpg"/>
-                  </Slider>
-                </div>
-              </div>
+              <FeaturedSlider />
+              <HottestSlider />
+              <BookedSlider />
             </div>
 
             <div className="electron">
