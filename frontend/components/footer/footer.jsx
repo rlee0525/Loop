40d5704f7194e9de34014f85bf4b0538cd922ff3
Raymond Lee <raylee0525@gmail.com>
2017-03-15 00:24:32 -0700

    Implement footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -4,7 +4,25 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
-        Loop, Inc.
+        <div className="footer-container">
+          <div className="footer-menu">
+            <div className="footer-vertical-menu">
+              English
+            </div>
+            <div className="footer-vertical-menu">
+              Loop
+            </div>
+            <div className="footer-vertical-menu">
+              Discover
+            </div>
+            <div className="footer-vertical-menu">
+              Hosting
+            </div>
+          </div>
+          <div className="footer-links">
+            Loop, Inc
+          </div>
+        </div>
       </div>
     );
   }
