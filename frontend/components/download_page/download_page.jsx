1c81b5ecfe35db6d558db62a82c3db93c191aa4d
Raymond Lee <raylee0525@gmail.com>
2017-03-15 00:18:52 -0700

    Complete downloads styling

--- frontend/components/download_page/download_page.jsx
+++ frontend/components/download_page/download_page.jsx
@@ -9,12 +9,8 @@ class DownloadPage extends React.Component {
           <img src="http://res.cloudinary.com/rlee0525/image/upload/v1489559864/electron_placeholder-min_oumsep.png" />
         </div>
         <div className="download-links">
-          <span>
-            Download Mobile App
-          </span>
-          <span>
-            Download Desktop App
-          </span>
+          <span>Download Mobile App</span>
+          <span>Download Desktop App</span>
         </div>
       </div>
     );
